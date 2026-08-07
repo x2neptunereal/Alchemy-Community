9b1ba9451a97a856bf4e52082d8f12198e7849f6
laypliss <mjmosti01+github@gmail.com>
2026-08-07 17:46:22 +0300

    Add Ancient Isle Pond location coordinates

--- fisch/locations.lua
+++ fisch/locations.lua
@@ -5,4 +5,5 @@ return {
     ["Roslit Bay"] = {-1450, 135, 750};
     ["Scoria Reach"] = {-5070, 145, -1510};
     ["Ancient Isle"] = {5833, 125, 401};
+    ["Ancient Isle Pond"] = {6010, 190, 131};
 }
