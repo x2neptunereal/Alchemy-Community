98984b5227cec879b24b07505dc2e54171ae0b34
x2Neptune <x2neptunereal@gmail.com>
2026-08-07 20:44:14 +0700

    Update locations.lua

--- fisch/locations.lua
+++ fisch/locations.lua
@@ -2,7 +2,7 @@ return {
     --format: ["NAME"] = {X, Y, Z};
     ["Example"] = {0, 0, 0};
   
-    ["Example"] = {0, 0, 0};
-    ["Example"] = {0, 0, 0};
-    ["Example"] = {0, 0, 0};
+    ["Example2"] = {0, 0, 0};
+    ["Example3"] = {0, 0, 0};
+    ["Example4"] = {0, 0, 0};
 }
