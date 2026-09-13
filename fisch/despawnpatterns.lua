b8b043875ee200aad67ae1111e11692e8e47398d
x2Neptune <x2neptunereal@gmail.com>
2026-09-13 23:06:59 +0700

    Create despawnpatterns.lua

---
+++ fisch/despawnpatterns.lua
@@ -0,0 +1,9 @@
+--- !!! do not edit this without permission !!! ---
+
+return {
+    "has fled", "has disappeared", "has vanished", "has retreated", "has left", "has been defeated",
+    "has despawned", "has returned to", "swam away", "into the depths", "no longer", "has calmed",
+    "has ended", "has subsided", "pools? have despawned", "event has ended", "Hunt has ended",
+    "Hunt ended", "has been caught", "has caught", "has perfectly caught", "has burrowed",
+    "got away", "gone", "sinks back into", "fades",
+}
