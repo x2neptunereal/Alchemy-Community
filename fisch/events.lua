71921f6016539113ddcc0aee59a92da0cd337268
x2Neptune <x2neptunereal@gmail.com>
2026-09-13 23:04:26 +0700

    Create events.lua

---
+++ fisch/events.lua
@@ -0,0 +1,22 @@
+--- !!! do not edit this without permission !!! ---
+
+return {
+    "Fin Whale Pool","Photic Terrosunder Pool","Terrosunder Pool","King Crabstle Hunt","Livyatan Hunt","Humpback Whale Pool","Megamouth Hunt","Sovereign Beam","Dreadfin Hunt","Rotbloom Hunt",
+    "Flower Guardian Hunt","Nectar Den - Serpent","Toxic Boil","Wyvern Hunt",
+    "Skeletal Leviathan Hunt","Brine Storm","Omnithal Hunt","Plesiosaur Hunt",
+    "Reef Titan Hunt","Pliosaur Hunt","Goldwraith Hunt","Frostwyrm Hunt",
+    "Baby Bloop Fish","Bloop Fish","The Sanctum Profane Hunt","The Sanctum Hunt","Cathulid Abundance","Cathulith Abundance",
+    "Eruption","Earthquake",
+    "Mosslurker","MossjawHunt",
+    "Colossal Ethereal Dragon","Colossal Ancient Dragon","Colossal Blue Dragon",
+    "The Depths - Serpent","Forsaken Veil - Scylla",
+    "Orcas","Orcas Pool","Whales Pool","Ancient Orcas Pool", "Orca Migration",
+    "Isonade","The Kraken Pool","Megalodon Default","Megalodon Ancient",
+    "Great White Shark","Great Hammerhead Shark","Whale Shark","Cursed Isle",
+    "Tidecrasher Archon Hunt","Helios Sunray Hunt","Styx Angler Hunt",
+    "Legionnaire Lamprey Hunt","Kerauno Wyrm Pool","Ancient Kraken Pool",
+    "Olympian Devil Hunt",
+    "Wisp Haunt","Soul Pool","Apollo's Beam Of Light","Poseidon's Whirlpool",
+    "Magician Narwhal - Sea 1","Beluga - Sea 1","Narwhal - Sea 1", "Abaia Hunt",
+    "Halibut Harpoon",
+}
