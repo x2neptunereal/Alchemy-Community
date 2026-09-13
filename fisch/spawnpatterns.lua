d18d43d86487a690ffa1fc4c71e58d7012a53b8b
x2Neptune <x2neptunereal@gmail.com>
2026-09-13 23:06:31 +0700

    Create spawnpatterns.lua

---
+++ fisch/spawnpatterns.lua
@@ -0,0 +1,8 @@
+--- !!! do not edit this without permission !!! ---
+
+return {
+    "has surfaced", "has been spotted", "has appeared", "has emerged", "has risen", "has arrived",
+    "is approaching", "has awakened", "has been summoned", "has invaded", "began to shake",
+    "has erupted", "has begun", "has started", "event has begun", "Hunt has begun",
+    "pools? have spawned", "has occurred", "has opened", "and summoned", "stirs in the waters",
+}
